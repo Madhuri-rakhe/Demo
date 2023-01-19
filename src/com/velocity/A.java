cd8c3c0d75dade2f46d510b41aa304ee8cb15ed5
madhuri <madhuriprakhe@gmail.com>
2023-01-19 12:44:44 +0530

    Demo project

---
+++ src/com/velocity/A.java
@@ -0,0 +1,19 @@
+package com.velocity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class A {
+
+	public static void main(String[] args) {
+		Set<Integer> set=new HashSet<Integer>();
+		set.add(1);
+		set.add(2);
+		set.add(3);
+		set.add(4);
+		for(Integer i :set) {
+			System.out.println(i);
+		}
+	}
+
+}
