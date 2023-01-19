563e24d596180e76435365a5e5f661465a489c5a
madhuri <madhuriprakhe@gmail.com>
2023-01-19 13:51:16 +0530

    new classes added

--- src/com/velocity/HashMapDemo.java
+++ src/com/velocity/HashMapDemo.java
@@ -1,9 +1,17 @@
 package com.velocity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapDemo {
 
+	
 	public static void main(String[] args) {
-		System.out.println("hello");
+	  Map<Integer,String> map=new HashMap<Integer,String>();
+	  map.put(101,"madhuri");
+	  map.put(102,"mayuri");
+	  map.put(103,"balaji");
+	  System.out.println(map);
 
 	}
 
